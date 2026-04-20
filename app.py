0e4ab2ef768ae9859df8ffb44c4f782cade339c5
MaddinPython <martin.suffa@gmail.com>
2026-04-20 15:59:59 +0200

    added create possibilty via form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import db_operations
 
 app = Flask(__name__)
@@ -9,6 +9,25 @@ def index():
     # add code here to fetch the job posts from a file
     blog_posts = db_operations.get_all_blogs()
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # We will fill this in the next step
+        print(request.form.get('author'))
+        print(request.form.get('title'))
+        print(request.form.get('content'))
+        # adding to "db", id handled in post_blog-func
+        new_blog_post = {
+            'author': request.form.get('author'),
+            'title': request.form.get('title'),
+            'content': request.form.get('content')
+        }
+        result = db_operations.post_blog(new_blog_post)
+        print(f'Added new blog_post with id "{result}".')
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
--- db_operations.py
+++ db_operations.py
@@ -2,6 +2,22 @@ import json
 
 PATH_TO_DB_FILE = './easy_db.json'
 
+
+def save_to_db(data):
+    with open(PATH_TO_DB_FILE, 'w') as json_file:
+        json.dump(data, json_file)
+
+
 def get_all_blogs():
     with open(PATH_TO_DB_FILE, "r") as json_file:
         return json.load(json_file)
+
+
+def post_blog(post):
+    current_collection = get_all_blogs()
+    current_max_id_post = max(current_collection, key=lambda x: x['id'])
+    new_post_id = current_max_id_post['id'] + 1
+    post['id'] = new_post_id
+    current_collection.append(post)
+    save_to_db(current_collection)
+    return new_post_id
